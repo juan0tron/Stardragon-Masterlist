572cd2a925c01c11f50cc972fbf59e35244387e8
juan0tron <juandglover@gmail.com>
2017-08-31 21:02:43 -0400

    Add Less compiler, create home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
+
+// Pages
+import { DashboardComponent } from './views/dashboard/dashboard.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
+    // Pages
+    DashboardComponent,
+
     AppComponent
   ],
   imports: [
