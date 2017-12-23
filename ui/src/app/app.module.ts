ff1e2fedb419e4254edef6eb4df92ec91eeedc4a
juan0tron <juandglover@gmail.com>
2017-12-22 17:49:59 -0800

    Install SweetAlert2 + ngx-sweetalert2

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,3 +1,4 @@
+// Angular Modules
 import { BrowserModule }    from '@angular/platform-browser';
 import { NgModule }         from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -8,6 +9,9 @@ import { DashboardComponent }         from './views/dashboard/dashboard.componen
 import { DiscordComponent }           from './views/discord/discord.component';
 import { StarDragonListComponent }    from './views/stardragons/stardragon-list.component';
 import { StarDragonDetailsComponent } from './views/stardragons/details/stardragon-details.component';
+
+// 3rd Party Modules
+import { SweetAlert2Module } from '@toverux/ngx-sweetalert2';
 
 import { ROUTES }       from "./app.routes";
 import { AppComponent } from './app.component';
@@ -23,9 +27,18 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    // Angular Modules
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+
+    // 3rd Party Modules
+    SweetAlert2Module.forRoot({
+        buttonsStyling: false,
+        customClass: 'modal-content',
+        confirmButtonClass: 'btn btn-primary',
+        cancelButtonClass: 'btn'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
