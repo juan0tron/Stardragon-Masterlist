d9fdf80fc2d25aedf767c97a37b0787972c71edb
Jon Glover <juandglover@gmail.com>
2018-03-03 18:20:41 -0800

    Add stardragon /list route

--- ui/src/app/routes/app.routes.ts
+++ ui/src/app/routes/app.routes.ts
@@ -12,8 +12,9 @@ import { LoginComponent } from "app/views/login/login.component";
 
 import { UserComponent } from "app/views/user/profile/user.component";
 
-// Species traits
-import { TraitsComponent } from 'app/views/stardragon/traits/traits.component';
+// Stardragons
+import { StardragonListComponent } from "app/views/stardragon/list/stardragon-list.component";
+import { TraitsComponent }         from 'app/views/stardragon/traits/traits.component';
 
 // Routing Guards
 import { LoggedInGuard } from "app/guards/logged-in.guard";
@@ -26,15 +27,16 @@ export const ROUTES: Routes = [
     // Public Pages
     { path: 'home',           component: DashboardComponent },
     { path: 'discord',        component: DiscordComponent   },
-    { path: 'termsofservice', component: TosComponent   },
-    { path: 'makeyourown',    component: MYOComponent   },
+    { path: 'termsofservice', component: TosComponent       },
+    { path: 'makeyourown',    component: MYOComponent, canActivate: [DevGuard]},
 
     { path: 'login', component: LoginComponent, canActivate: [DevGuard]},
 
     { path: 'users',          component: UserComponent,  canActivate: [DevGuard]},
     { path: 'users/:user_id', component: UserComponent,  canActivate: [DevGuard]},
 
-    // Species Traits Sheet
+    // Stardragons
+    { path: 'stardragons',           component: StardragonListComponent,  canActivate: [DevGuard]},
     { path: 'species/:species_name', component: TraitsComponent },
 
     // Handle all other routes
