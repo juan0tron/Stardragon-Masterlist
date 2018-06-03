b89c5b0ba6dff35a27748505466f4aceb120e5cd
Jon Glover <juandglover@gmail.com>
2018-06-02 22:53:54 -0700

    Wrestled with npm for 3 years and updated to Angular 6

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # The Gem Exchange
 
 This site will be the future home for everything related to the closed species Stardragons. Built on a MEAN stack - MongoDB, Express, Angular 4, and Node.js.
-
-# To Do
-
-Progress for the site can be tracked on this [Trello board](https://trello.com/b/htEnUyCt). 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,9 +28,10 @@ import { RegisterComponent }  from "./views/register/register.component";
 import { NavigationComponent } from "./views/navigation/navigation.component";
 
 // 3rd Party Modules
-import { FacebookModule }    from 'ngx-facebook';
-import { SweetAlert2Module } from '@toverux/ngx-sweetalert2';
-import { ButtonsModule }     from 'ngx-bootstrap';
+import { FacebookModule }     from 'ngx-facebook';
+import { SweetAlert2Module }  from '@toverux/ngx-sweetalert2';
+import { ButtonsModule }      from 'ngx-bootstrap';
+import { NgxTypeaheadModule } from 'ngx-typeahead';
 
 // Routing Guards
 import { LoggedInGuard } from "./guards/logged-in.guard";
@@ -85,6 +86,7 @@ import { AppComponent } from './app.component';
     }),
     // NGX Bootstrap
     ButtonsModule.forRoot(),
+    NgxTypeaheadModule
 
   ],
   providers: [
