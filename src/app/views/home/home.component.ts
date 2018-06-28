b10b32334deed62b221cff3e89b66b41384b7b7e
Jon Glover <juandglover@gmail.com>
2018-06-27 22:29:47 -0700

    Style home page buttons so they look nice

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -6,4 +6,37 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   title = 'Welcome to The Gem Exchange!';
+
+  links = [
+    {
+      "name":"Patreon",
+      "class":"fab fa-patreon",
+      "link":"https://www.patreon.com/GemExchange"
+    },
+    {
+      "name":"Toyhouse",
+      "class":"fa fa-home",
+      "link":"http://toyhou.se/~world/1420.gemexchange-jo-arca"
+    },
+    {
+      "name":"Fur Affinity",
+      "class":"fa fa-paw",
+      "link":"https://www.furaffinity.net/user/gemexchange"
+    },
+    {
+      "name": "Twitter",
+      "class":"fab fa-twitter",
+      "link":"https://twitter.com/Gem_Exchange"
+    },
+    {
+      "name":"Github",
+      "class":"fab fa-github",
+      "link":"https://github.com/juan0tron/the-gem-exchange"
+    },
+    {
+      "name":"DeviantArt",
+      "class":"fab fa-deviantart",
+      "link":"https://gemexchange.deviantart.com/"
+    },
+  ];
 }
