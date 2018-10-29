8afea7fc2c717c2d7321dad10a869befa50432fc
Jon Glover <juandglover@gmail.com>
2018-10-28 20:47:05 -0700

    Scrapped JS implementation of parallax and rewrote it in pure CSS

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -45,31 +45,6 @@ export class HomeComponent {
     }
   ];
 
-  layers:Array<HTMLElement> = [];
-
-  constructor(){
-    // Build the layers array once on page init
-    this.layers = <HTMLElement[]><any>document.getElementsByClassName("parallax-layer");
-  }
-
-  // parallax scroll effect
-  @HostListener('window:scroll', [])
-  onWindowScroll(ev:KeyboardEvent) {
-    window.requestAnimationFrame(() => {
-      let topDistance = window.pageYOffset;
-      for (let layer of this.layers) {
-        const depth:any   = layer.getAttribute('data-depth');
-        const movement    = -(topDistance * depth);
-        const translate3d = `translate3d(0, ${movement}px, 0)`;
-        (layer as HTMLElement).style['-webkit-transform'] = translate3d;
-        (layer as HTMLElement).style['-moz-transform']    = translate3d;
-        (layer as HTMLElement).style['-ms-transform']     = translate3d;
-        (layer as HTMLElement).style['-o-transform']      = translate3d;
-        (layer as HTMLElement).style.transform            = translate3d;
-      }
-    });
-  }
-
   // Lazy load animation helpers
   showLayer(index){
     document.getElementById('layer-'+index).style.animation  = 'fadeIn 2s';
