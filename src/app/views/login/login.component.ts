fed6fc68aee55ec9b5aa78a6341f31f1a5187cb2
Jon Glover <juandglover@gmail.com>
2018-11-01 08:50:53 -0700

    Show errors in login component

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -17,13 +17,44 @@ export class LoginComponent {
   public password: string;
   public error:    string;
 
+  public email_invalid:boolean    = false;
+  public password_invalid:boolean = false;
+
   constructor(
     public  auth:   AuthService,
     private router: Router
   ) {}
 
   login(){
-    this.auth.login(this.email, this.password);
+    if(!this.email){
+      this.email_invalid = true;
+    }
+    else if(!this.password){
+      this.password_invalid = true;
+    }
+    else{
+      this.auth.login(this.email, this.password)
+        .subscribe(
+          data => {
+            this.router.navigate['/dashboard'];
+          },
+          err  => {
+            this.error = err.message;
+            if(err.message == "Invalid email." || err.message == "Email is required."){
+              this.email_invalid    = true;
+              this.password_invalid = false;
+            }
+            else if(err.message == "Invalid password."){
+              this.email_invalid    = false;
+              this.password_invalid = true;
+            }
+            else{
+              this.email_invalid    = false;
+              this.password_invalid = false;
+            }
+          }
+        );
+    }
   }
 
 }
